3b1ae5a7e5133960ed49f474a6751ff78fb6a5d9
kvvba <96693127+kvvba@users.noreply.github.com>
2022-06-27 10:11:47 +0100

    Readme update, MIT -> GPL3

--- init.el
+++ init.el
@@ -1,24 +1,19 @@
 ;;;; orbmacs --- Emacs configuration for org mode
 
-;; Copyright (c) 2022 jakub@posteo.net
+    ;; Copyright (C) 2022  jakub@posteo.net
 
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "software"), to deal
-;; in the software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the software, and to permit persons to whom the software is
-;; furnished to do so, subject to the following conditions:
+    ;; This program is free software: you can redistribute it and/or modify
+    ;; it under the terms of the GNU General Public License as published by
+    ;; the Free Software Foundation, either version 3 of the License, or
+    ;; (at your option) any later version.
 
-;; The above copyright notice and this permission notice shall be included in all
-;; copies or substantial portions of the Software.
+    ;; This program is distributed in the hope that it will be useful,
+    ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ;; GNU General Public License for more details.
 
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;; SOFTWARE.
+    ;; You should have received a copy of the GNU General Public License
+    ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; Orbmacs is an Emacs configuration with a focus on org mode and "living in Emacs"
